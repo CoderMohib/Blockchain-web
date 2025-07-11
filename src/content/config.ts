9d0f91054c428dc9b39228d16bea25c1a3271028
CoderMohib <alimohib025@gmail.com>
2025-07-12 00:10:13 +0500

    Fix:Careers folder error!

--- src/content/config.ts
+++ src/content/config.ts
@@ -28,6 +28,13 @@ const careers = defineCollection({
 		type: z.string(),
 	}),
 });
-
-export const collections = { blog, careers };
+const positions = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		category: z.string(),
+	}),
+});
+export const collections = { blog, careers,positions };
 
